c0302603078786cdfa4c3db84faedebe4d843d80
harshrawat20 <77675473+harshrawat20@users.noreply.github.com>
2021-11-21 15:28:20 +0530

    Change i port

--- server/services/render.js
+++ server/services/render.js
@@ -3,11 +3,12 @@ const axios=require('axios');
 
 exports.homeRoutes=(req,res)=>{
 // Make a get request to /api/uers
-    axios.get('http://localhost:3000/api/users')
+    axios.get('http://localhost:8080/api/users')
     .then(function(response){
         res.render('index',{users:response.data});
     })
     .catch(err=>{
+        console.log(" axios catch error ")
         res.send(err);
     })
 }
@@ -17,7 +18,7 @@ exports.add_user=(req,res)=>{
 }
 
 exports.update_user=(req,res)=>{
-    axios.get('http://localhost:3000/api/users',{params:{id:req.query.id}})
+    axios.get('http://localhost:8080/api/users',{params:{id:req.query.id}})
       .then(function(userdata){
           res.render("update_user",{user:userdata.data})
       })
