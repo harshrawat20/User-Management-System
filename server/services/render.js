0b3531bd790813da21f6e13b5dbba6d48f5e7cea
harshrawat20 <77675473+harshrawat20@users.noreply.github.com>
2021-11-21 15:31:29 +0530

    Update render.js

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,7 @@ const axios=require('axios');
 
 exports.homeRoutes=(req,res)=>{
 // Make a get request to /api/uers
-    axios.get('http://localhost:8080/api/users')
+    axios.get('https://user-manage-system.herokuapp.com/api/users')
     .then(function(response){
         res.render('index',{users:response.data});
     })
@@ -18,7 +18,7 @@ exports.add_user=(req,res)=>{
 }
 
 exports.update_user=(req,res)=>{
-    axios.get('http://localhost:8080/api/users',{params:{id:req.query.id}})
+    axios.get('https://user-manage-system.herokuapp.com/api/users',{params:{id:req.query.id}})
       .then(function(userdata){
           res.render("update_user",{user:userdata.data})
       })
